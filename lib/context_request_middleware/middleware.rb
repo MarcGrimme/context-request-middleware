3f9b8e46d36f319f50ac190a47c32816d55d04f7
Alexander Tomov <Tomov-ext@fidor.com>
2019-11-22 14:21:27 +0200

    Add default request start time.

--- lib/context_request_middleware/middleware.rb
+++ lib/context_request_middleware/middleware.rb
@@ -101,7 +101,7 @@ module ContextRequestMiddleware
       ContextRequestMiddleware.select_request_headers(
         ContextRequestMiddleware.request_start_time_headers,
         request
-      )
+      ) || Time.current
     end
 
     def source(request)
